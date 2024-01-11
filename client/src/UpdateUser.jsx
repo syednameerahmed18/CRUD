ffa19c3037d9dd9d42a8f987ac294f373fbd2147
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-01-11 21:01:40 +0530

    Update Method

--- client/src/UpdateUser.jsx
+++ client/src/UpdateUser.jsx
@@ -1,22 +1,51 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import {useParams, useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 function UpdateUser() {
+    const {id} = useParams()
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [age, setAge] = useState()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/getUser/" +id)
+        .then(result => {console.log(result)
+            setName(result.data.name)
+            setEmail(result.data.email)
+            setAge(result.data.age)
+        })
+        .catch(err => console.log(err))
+
+    },[])   
+
+    const Update = (e) => {
+        e.preventDefault()
+        axios.put("http://localhost:3001/updateUser/"+id, {name, email, age})
+        .then(result => {
+            console.log(result)
+            navigate('/')
+        })
+        .catch(err => console.log(err))
+    }
+
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
         <div className='w-50 bg-white rounded p-3'>
-            <form>
+            <form onSubmit={Update}>
                 <h2>Update User</h2>
                 <div className='mb-2'>
                     <label htmlFor=''>Name</label>
-                    <input type='text' placeholder='Enter Name' className='form-control'></input>
+                    <input type='text' placeholder='Enter Name' className='form-control' value={name} onChange={(e) => setName(e.target.value)}></input>
                 </div>
                 <div className='mb-2'>
                     <label htmlFor=''>Email</label>
-                    <input type='text' placeholder='Enter Email' className='form-control'></input>
+                    <input type='text' placeholder='Enter Email' className='form-control' value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 </div>
                 <div className='mb-2'>
                     <label htmlFor=''>Age</label>
-                    <input type='text' placeholder='Enter Age' className='form-control'></input>
+                    <input type='text' placeholder='Enter Age' className='form-control' value={age} onChange={(e) => setAge(e.target.value)}></input>
                 </div>
                 <button className='btn btn-success'>Submit</button>
             </form>
--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -10,7 +10,7 @@ function Users() {
         .then(result => setUsers(result.data))
         .catch(err => console.log(err))
 
-    },[]);
+    },[])
   return (
     <div className ="d-flex vh-100 bg-primary justify-content-center align-items-center">
         <div className='w-50 bg-white rounded p-3'>
@@ -32,7 +32,7 @@ function Users() {
                                     <td>{user.email}</td>
                                     <td>{user.age}</td>
                                     <td>
-                                        <Link to="/update" className='btn btn-success'>Update</Link>
+                                        <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link>
                                         <button className='btn btn-danger'>Delete</button>
                                     </td>
                             </tr>
