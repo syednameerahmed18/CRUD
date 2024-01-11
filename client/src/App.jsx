84b4e7567231dd859cc865d5855c76e936153762
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-01-11 15:09:17 +0530

    My First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Users from './Users';
+import CreateUser from './CreateUser';
+import UpdateUser from './UpdateUser';
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Users/>}></Route>
+          <Route path='/create' element={<CreateUser/>}></Route>
+          <Route path='/update' element={<UpdateUser/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/Users.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Users() {
+    const [users, setUsers] = useState([{
+        Name: "Syed", Email: "syednameerahmed@gmail.com", Age: 23
+    }])
+  return (
+    <div className ="d-flex vh-100 bg-primary justify-content-center align-items-center">
+        <div className='w-50 bg-white rounded p-3'>
+            <Link to="/create" className='btn btn-success'>Add +</Link>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user) => {
+                            return <tr>
+                                <td>{user.Name}</td>
+                                <td>{user.Email}</td>
+                                <td>{user.Age}</td>
+                                <td><button>Edit</button><button>Delete</button></td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
+
+export default Users
