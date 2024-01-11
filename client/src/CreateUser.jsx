6a3654fdc9619d24d2020f4a42eebd479790fe80
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-01-11 16:26:11 +0530

    Connection from frontend to backend

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function CreateUser() {
-  return (
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [age, setAge] = useState();
+
+    const Submit = (e) => {
+        e.preventDefault()
+        axios.post("http://localhost:3001/createUser", {name, email, age})
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+    return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
         <div className='w-50 bg-white rounded p-3'>
-            <form>
+            <form onSubmit={Submit}>
                 <h2>Add User</h2>
                 <div className='mb-2'>
                     <label htmlFor=''>Name</label>
-                    <input type='text' placeholder='Enter Name' className='form-control'></input>
+                    <input type='text' placeholder='Enter Name' className='form-control' onChange={(e) => setName(e.target.value)}></input>
                 </div>
                 <div className='mb-2'>
                     <label htmlFor=''>Email</label>
-                    <input type='text' placeholder='Enter Email' className='form-control'></input>
+                    <input type='text' placeholder='Enter Email' className='form-control' onChange={(e) => setEmail(e.target.value)}></input>
                 </div>
                 <div className='mb-2'>
                     <label htmlFor=''>Age</label>
-                    <input type='text' placeholder='Enter Age' className='form-control'></input>
+                    <input type='text' placeholder='Enter Age' className='form-control' onChange={(e) => setAge(e.target.value)}></input>
                 </div>
                 <button className='btn btn-success'>Submit</button>
             </form>
