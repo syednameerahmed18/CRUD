e89fa12e42b8cc1756689c8ff34e8ddd0dda776e
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-01-11 16:34:04 +0530

    .

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
 function CreateUser() {
     const [name, setName] = useState();
     const [email, setEmail] = useState();
     const [age, setAge] = useState();
+    const navigate = useNavigate()
 
     const Submit = (e) => {
         e.preventDefault()
         axios.post("http://localhost:3001/createUser", {name, email, age})
-        .then(result => console.log(result))
+        .then(result => {
+            console.log(result)
+            navigate('/')
+        })
         .catch(err => console.log(err))
     }
     return (
