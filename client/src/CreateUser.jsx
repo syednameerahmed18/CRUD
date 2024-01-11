12d9ef418f1c468d406ac847ca6f1ce093b29ef1
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-01-11 15:21:33 +0530

    Made changes

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CreateUser() {
   return (
-    <div className='d flex vh-100 bg-primary justify-content-center align-items-center'>
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
         <div className='w-50 bg-white rounded p-3'>
             <form>
                 <h2>Add User</h2>
